e771d770a00fb6d94862ea7d01c8345d431c0dd2
Ivar Bredeli <ivabr@stud.ntnu.no>
2024-11-08 11:27:18 +0100

    Changes to suck/blow mechanic

--- Assets/_Scripts/Player/Ability/PlayerSuckAndBlow.cs
+++ Assets/_Scripts/Player/Ability/PlayerSuckAndBlow.cs
@@ -6,60 +6,65 @@ public class PlayerSuckAndBlow : MonoBehaviour {
 
     private HashSet<Rigidbody> pullableObjectsInZone = new HashSet<Rigidbody>();
     private bool isHolding = false;
-    private bool isSuckForcedApplied = false;
+    private bool isPullForcedApplied = false;
+    private bool isPushForcedApplied = false;
+    private bool isHoldingWaitTimeFinished = true;
 
-    [SerializeField]
-    private PlayerSuckPickUp playerSuckPickUp;
-
-    [SerializeField]
-    private Transform pickUpLocation;
-
-    [SerializeField]
-    private InputManager inputManager;
+    [SerializeField] private float addedForce = 50.0f;
+    [SerializeField] private PlayerSuckPickUp playerSuckPickUp;
+    [SerializeField] private Transform pickUpLocation;
+    [SerializeField] private InputManager inputManager;
     void Update() {
-        if (!isSuckForcedApplied && inputManager.isSucking && !isHolding) {
-            isSuckForcedApplied = true;
+        if (this.inputManager.isSucking && !this.isHolding) {
             foreach (Rigidbody pickableRigidBody in playerSuckPickUp.pickUpObjectsInZone) {
-                if (!isHolding) {
-                    Debug.Log($"{pickableRigidBody.gameObject.name} picked up");
-                    isHolding = true;
+                if (!this.isHolding) {
+                    //Debug.Log($"{pickableRigidBody.gameObject.name} picked up");
+                    this.isHolding = true;
                     PickUp(pickableRigidBody);
                 } else {
                     AppliePullForce(pickableRigidBody);
                 }
             }
 
-            foreach (Rigidbody pullableRigidBody in pullableObjectsInZone) {
+            foreach (Rigidbody pullableRigidBody in this.pullableObjectsInZone) {
                 AppliePullForce(pullableRigidBody);
             }
-        } else if (!inputManager.isSucking) {
-            isSuckForcedApplied = false;
+            this.isPullForcedApplied = true;
+        } else {
+            this.isPullForcedApplied = false;
         }
 
-        if (inputManager.isBlowing) {
+        if (this.inputManager.isBlowing) {
 
+            float effectiveForce = this.isPushForcedApplied ? this.addedForce : this.addedForce * 0.8f;
             if (HasPickUp()) {
-                GameObject childGameObject = pickUpLocation.GetChild(0).gameObject;
-                Rigidbody childRb = childGameObject.GetComponent<Rigidbody>();
-                ContactPointController cp = childGameObject.GetComponentInChildren<ContactPointController>();
+                DropPickUpItem();
+                this.isHolding = false;
+                this.isHoldingWaitTimeFinished = false;
+                StartCoroutine(PushWaitTime(0.5f));
 
-                childGameObject.transform.SetParent(null);
-                childRb.isKinematic = false;
-
-                // Appling Push Force
-                if (cp != null) {
-                    childRb.AddForce(transform.forward, ForceMode.Impulse);
-                } else {
-                    childRb.AddForce(transform.forward, ForceMode.Impulse);
-                }
-                isHolding = false;
-            } else {
+            } else if (this.isHoldingWaitTimeFinished) {
                 // Applies the push force
-                foreach (Rigidbody pullableRigidBody in pullableObjectsInZone) {
-                    pullableRigidBody.AddForce(transform.forward, ForceMode.Impulse);
+                foreach (Rigidbody pullableRigidBody in this.pullableObjectsInZone) {
+                    pullableRigidBody.AddForce(transform.forward * effectiveForce, ForceMode.Force);
+                    Debug.Log("Pushing");
                 }
             }
+            this.isPushForcedApplied = true;
+        } else {
+            this.isPushForcedApplied = false;
+            this.isHoldingWaitTimeFinished = true;
         }
+
+    }
+
+    private ContactPointController DropPickUpItem() {
+        GameObject childGameObject = this.pickUpLocation.GetChild(0).gameObject;
+        Rigidbody childRb = childGameObject.GetComponent<Rigidbody>();
+        ContactPointController cp = childGameObject.GetComponentInChildren<ContactPointController>();
+        childGameObject.transform.SetParent(null);
+        childRb.isKinematic = false;
+        return cp;
     }
 
     private void PickUp(Rigidbody pickableRigidBody) {
@@ -83,13 +88,14 @@ public class PlayerSuckAndBlow : MonoBehaviour {
         if (cp != null) {
             direction = (pickUpLocation.position - cp.GetContactPoint().position).normalized;
         }
-        pullableRigidBody.AddForce(direction, ForceMode.Impulse);
+        float effectiveForce = isPullForcedApplied ? addedForce * 0.8f : addedForce;
+        pullableRigidBody.AddForce(direction * effectiveForce, ForceMode.Force);
     }
 
     private void OnTriggerEnter(Collider collider) {
         if ((collider.CompareTag("Suckable") || collider.CompareTag("SuckAndPickup")) && collider.attachedRigidbody != null) {
             pullableObjectsInZone.Add(collider.attachedRigidbody);
-            Debug.Log($"In suck zone, {collider.gameObject.name}");
+            //Debug.Log($"In suck zone, {collider.gameObject.name}");
 
             ContactPointController cp = collider.GetComponentInChildren<ContactPointController>();
             if (cp != null) {
@@ -101,7 +107,7 @@ public class PlayerSuckAndBlow : MonoBehaviour {
     private void OnTriggerExit(Collider collider) {
         if (pullableObjectsInZone.Contains(collider.attachedRigidbody)) {
             pullableObjectsInZone.Remove(collider.attachedRigidbody);
-            Debug.Log($"Left suck zone, {collider.gameObject.name}");
+            //Debug.Log($"Left suck zone, {collider.gameObject.name}");
 
             ContactPointController cp = collider.GetComponentInChildren<ContactPointController>();
             if (cp != null) {
@@ -136,9 +142,13 @@ public class PlayerSuckAndBlow : MonoBehaviour {
         pullableRigidBody.MovePosition(pickUpLocation.position);
     }
 
-
     public bool HasPickUp() {
         return pickUpLocation.childCount > 0;
+    }
+
+    private IEnumerator PushWaitTime(float seconds) {
+        yield return new WaitForSeconds(seconds);
+        this.isHoldingWaitTimeFinished = true;
     }
 
 }
--- Assets/_Scripts/Player/Ability/PlayerSuckPickUp.cs
+++ Assets/_Scripts/Player/Ability/PlayerSuckPickUp.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSuckPickUp : MonoBehaviour
-{
+public class PlayerSuckPickUp : MonoBehaviour {
     public HashSet<Rigidbody> pickUpObjectsInZone { get; private set; } = new HashSet<Rigidbody>();
 
     private void OnTriggerEnter(Collider collider) {
         if (collider.CompareTag("SuckAndPickup") && collider.attachedRigidbody != null) {
             pickUpObjectsInZone.Add(collider.attachedRigidbody);
-            Debug.Log($"In pick up zone, {collider.gameObject.name}");
+            //Debug.Log($"In pick up zone, {collider.gameObject.name}");
         }
     }
 
     private void OnTriggerExit(Collider collider) {
         if (pickUpObjectsInZone.Contains(collider.attachedRigidbody)) {
             pickUpObjectsInZone.Remove(collider.attachedRigidbody);
-            Debug.Log($"Left pick up zone, {collider.gameObject.name}");
+            //Debug.Log($"Left pick up zone, {collider.gameObject.name}");
         }
     }
 }
