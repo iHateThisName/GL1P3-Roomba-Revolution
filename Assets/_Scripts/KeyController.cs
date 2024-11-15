b8aa28626744725c16bf5e2b49003ebfa1e62bb5
mbl <mikaellaegran@gmail.com>
2024-11-15 10:55:56 +0100

    cleaned hierarchy

--- Assets/_Scripts/KeyController.cs
+++ Assets/_Scripts/KeyController.cs
@@ -1,12 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.ProBuilder.Shapes;
 
 public class KeyController : MonoBehaviour {
 
     [SerializeField] private DoorController doorController;
+    [SerializeField] private GameObject roomba;
+    [SerializeField] private BoxCollider bc;
+    //private Collider keyCollider;
+    private Rigidbody rb; 
 
     void Start() {
-
+        //keyCollider = GetComponent<Collider>();
+        //rb.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,9 +21,17 @@ public class KeyController : MonoBehaviour {
     }
 
     private void OnCollisionEnter(Collision collision) {
+
         if (collision.gameObject.CompareTag("Door") && collision.gameObject == doorController.gameObject) {
             doorController.Unlock();
             Destroy(gameObject);
         }
+
+        if (collision.gameObject.CompareTag("Player")) {
+            rb.isKinematic = true;
+            bc.enabled = false;
+        }
     }
+
+
 }
