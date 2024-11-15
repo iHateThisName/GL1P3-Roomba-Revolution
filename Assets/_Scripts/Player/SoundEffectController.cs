6c514a6c7f08d7d72d50b23320f2b87b2c0ba00f
ValdemarHermannss <valdi04h@gmail.com>
2024-11-15 14:36:12 +0100

    Blow sound effect added and reorganized it in the player prefab

--- Assets/_Scripts/Player/SoundEffectController.cs
+++ Assets/_Scripts/Player/SoundEffectController.cs
@@ -4,12 +4,16 @@ public class SoundEffectController : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
     private bool haveSucked = false;
+    private bool haveBlown = false;
 
     [Header("Audio")]
     [SerializeField] private AudioSource suckStart;
     [SerializeField] private AudioSource suckContinue;
     [SerializeField] private AudioSource suckEnd;
     [SerializeField] private AudioSource suckAttach;
+    [SerializeField] private AudioSource blowStart;
+    [SerializeField] private AudioSource blowContinue;
+    [SerializeField] private AudioSource blowEnd;
 
 
 
@@ -31,5 +35,22 @@ public class SoundEffectController : MonoBehaviour
             suckEnd.Play();
             haveSucked = false;
         }
+        if (isBlowing && !this.blowStart.isPlaying && !haveBlown)
+        {
+            blowStart.Play();
+            haveBlown = true;
+        }
+        else if (isBlowing && !blowStart.isPlaying && !blowContinue.isPlaying && haveBlown)
+        {
+            blowContinue.Play();
+        }
+        else if (!isBlowing && haveBlown)
+        {
+            blowStart.Stop();
+            blowContinue.Stop();
+            blowEnd.Play();
+            haveBlown = false;
+        }
+
     }
 }
