4842d8f46f91a85b9e0a925a1018cee350fdc137
mbl <mikaellaegran@gmail.com>
2024-12-02 13:27:17 +0100

    Helper

--- Assets/_Scripts/MainMenuScript.cs
+++ Assets/_Scripts/MainMenuScript.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -18,4 +20,17 @@ public class MainMenuController : MonoBehaviour {
     public void OnLoadLevel05() => GameManager.Instance.LoadScene(EnumScene.Level05);
 
     public void OnLoadMainMenu() => GameManager.Instance.LoadScene(EnumScene.MainMenu);
+    //public void OnLoadSettingsScene() => GameManager.Instance.LoadScene(EnumScene.SettingsScene);
+
+    public void OnLoadSettingsScene(){
+        //Helper.currentScene = EnumScene(SceneManager.GetActiveScene().buildIndex);
+        //int currentScene = SceneManager.GetActiveScene().buildIndex;
+        Helper.currentScene = (EnumScene)SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(Helper.currentScene);
+        GameManager.Instance.LoadScene(EnumScene.SettingsScene);
+    } 
+
+    public void OnBack() {
+        GameManager.Instance.LoadScene(Helper.currentScene);
+    }
 }
---
+++ Assets/_Scripts/Utility/Helper.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Helper
+{
+    public static EnumScene currentScene;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+   
+}
